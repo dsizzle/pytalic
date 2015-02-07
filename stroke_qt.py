212ab528128eaa92094c5e2b9153a5605c01ff8e
dsizzle <dcieslak@yahoo.com>
2015-02-06 23:55:35 -0800

    adjusted import of polygon to shapes.polygon
    Reordered imports for consistency

--- stroke_qt.py
+++ stroke_qt.py
@@ -3,16 +3,16 @@
 # stroke class definitions
 #
 #
-import splines
-import polygon
-import nibs_qt
-import control_vertex
-import serif
 
 import math
 import time
 import random
 
+import shapes.splines
+import shapes.polygon
+import nibs_qt
+import control_vertex
+import serif
 
 from PyQt4 import QtCore, QtGui
 
